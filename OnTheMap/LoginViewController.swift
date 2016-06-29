5d9abc53f028dae1d46423282e548723a284c99b
yuanjun221 <yuanjun221@gmail.com>
2016-06-29 14:01:05 +0800

    optimize login view

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -14,10 +14,13 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var onTheMapLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var socialView: UIView!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
     
-    @IBOutlet weak var signInWithLabel: UILabel!
-    @IBOutlet weak var facebookButton: UIButton!
     
+    
+    private var isSocialViewHidden = true
 }
 
 
@@ -33,6 +36,7 @@ extension LoginViewController {
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         subscribeToKeyboardNotifications()
+        loginButton.layer.cornerRadius = 2.0
     }
     
     override func viewWillDisappear(animated: Bool) {
@@ -42,6 +46,10 @@ extension LoginViewController {
     
     override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
         return UIInterfaceOrientationMask.Portrait
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
     }
 }
 
@@ -62,37 +70,49 @@ extension LoginViewController {
 
 // MARK: - View Shifting Behavior
 extension LoginViewController {
-    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
-        let userInfo = notification.userInfo
-        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
-        return keyboardSize.CGRectValue().height
+    func keyboardWillShow() {
+        view.frame.origin.y = -60
+        onTheMapLabel.frame.origin.y = 10
+        UIView.animateWithDuration(0.25, animations: {
+            self.signUpButton.alpha = 0
+        })
+
     }
     
-    func keyboardWillShow(notification: NSNotification) {
-        
-        if passwordTextField.isFirstResponder() {
-            view.frame.origin.y = -50
-        }
-        if emailTextField.isFirstResponder() {
-            view.frame.origin.y = 0
-        }
-        
-        
-    }
-    
-    func keyboardWillHide(notification: NSNotification) {
+    func keyboardWillHide() {
         view.frame.origin.y = 0
+        onTheMapLabel.frame.origin.y = 46
+        UIView.animateWithDuration(0.25, animations: {
+            self.signUpButton.alpha = 1
+        })
     }
     
     func subscribeToKeyboardNotifications() {
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillShow), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillHide), name: UIKeyboardWillHideNotification, object: nil)
     }
     
     func unsubsribeFromKeyboardNotifications() {
         NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
         NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
     }
+}
+
+
+// MARK: - Buttons Action
+extension LoginViewController {
+
+    @IBAction func toggleSocialView(sender: AnyObject) {
+        setSocialViewOffsetBy(isSocialViewHidden)
+    }
+    
+    func setSocialViewOffsetBy(isHidden: Bool) {
+        UIView.animateWithDuration(0.3, animations: {
+            self.socialView.frame.origin.y += isHidden ? -46 : 46
+        })
+        isSocialViewHidden = !isHidden
+    }
+    
 }
 
 
